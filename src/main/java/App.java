f091b99f93581c2fec8cb5547a6cd546b51f204e
Danilo <danilo.justino1@gmail.com>
2023-09-26 23:34:04 -0300

    Change Folder Structure

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class LoginJava {
+public class App {
     public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         List<String> listaEmail = new ArrayList<>();
