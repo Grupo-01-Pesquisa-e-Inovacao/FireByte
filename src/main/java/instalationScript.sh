ef00ee2e94acbdd51268d58dba7fb9bb722d5bc5
Danilo <danilo.pedrazzi@sptech.school>
2023-11-07 17:22:01 -0300

    InstalationScriptV1

--- src/main/java/instalationScript.sh
+++ src/main/java/instalationScript.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 #Update System
+echo "\033[0;34mAtualizando Sistema..."
 sudo apt update -y
 sudo apt upgrade -y
 
+#Create Containers
+#MySQL
+echo "\033[0;34mConfigurando MySQL Container"
+mkdir sql
+cd sql
+wget "https://raw.githubusercontent.com/NetMinder-Enterprise/FireByte-Backend/main/src/main/java/entities/Script%20DB.sql"
+cd ..
+touch Dockerfile
+wget "
+docker build -t firebytedb .
+docker run -d --name localDB -p 3306:3306 firebytedb
 #Java
-java -version
-if [ $? != 0 ];
-	then sudo apt install openjdk-17-jre -y
-fi
-mvn -v
-if [ $? != 0 ];
-	then sudo apt install maven -y
-fi
-
-#Repository
-git clone https://github.com/NetMinder-Enterprise/FireByte-backend
-cd FireByte-backend
-sudo mvn install #Dependencies
-sudo mvn assembly:single #Build
-cd target
-sudo java -jar login-java-netminder-1.1-jar-with-dependencies.jar
