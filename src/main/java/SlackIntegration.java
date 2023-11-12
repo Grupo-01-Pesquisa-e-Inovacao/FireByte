8f4c00f12d8814ac8cedbb96d76b59b71b6334fc
Danilo <danilo.justino1@gmail.com>
2023-11-12 17:22:07 -0300

    Fix Slack Token

--- src/main/java/SlackIntegration.java
+++ src/main/java/SlackIntegration.java
@@ -10,10 +10,11 @@ public class SlackIntegration {
         var client = Slack.getInstance().methods();
         try {
             var result = client.chatPostMessage(r -> r
-                .token("xoxb-6182782987507-6182807909619-1XIQXd2P61XmtIhyMbFigTZI")
+                .token("xoxb-6182782987507-6182807909619-sAmbhYu2bkWyj7L0OHNlTujp")
                 .channel(id)
                 .text(text)
             );
+            //System.out.println(result);
             System.out.println(String.format("Mensagem enviada ao Slack: %s", text));
         } catch (IOException | SlackApiException e) {
             System.out.println("Algo deu errado ao mandar mensagem no Slack!");
